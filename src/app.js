35ad07423e069a62460f025f7779ccc0623fe5e0
Ulifly <ulifly@hotmail.com>
2026-04-07 19:51:04 -0600

    servidor basico con express creado

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("Hola clase");
+import express from 'express';
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Ruta de prueba
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, Bartola ahi te dejo estos dos pesos!');
+});
+
+// iniciar el servidor
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando 😉 en http://localhost:${PORT}`);
+});
